0a3bf938940041e79b8d8bfc43f31b0351417024
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2024-09-28 16:01:56 +0100

    protos

--- build.rs
+++ build.rs
@@ -1,19 +1,30 @@
 use protox::prost::Message;
-use std::{env, fs, path::PathBuf};
+use std::{env, fs, path::PathBuf, process::Command};
 
-fn main() {
-    println!("cargo:rerun-if-changed=migrations");
-    let file_descriptors = protox::compile(["occurrence/v1/occurrences_service.proto"], ["./protos"]).unwrap();
+const PROTO_DOWNLOAD: &str = "___protos";
+const PROTO_INCLUDES: [&str; 1] = [PROTO_DOWNLOAD];
+const PROTO_FILES: [&str; 1] = ["occurrence/v1/occurrences_service.proto"];
 
-    let file_descriptor_path = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR not set"))
-        .join("file_descriptor_set.bin");
-    fs::write(&file_descriptor_path, file_descriptors.encode_to_vec()).unwrap();
+const BUF_SCHEMA: &str = "buf.build/zizico2/prociv-reverse-proxy:228c470f729042b69cf6b9360e2bad4b";
+
+fn main() -> anyhow::Result<()> {
+    Command::new("buf")
+        .args(["export", BUF_SCHEMA])
+        .args(["--output", PROTO_DOWNLOAD])
+        .output()?;
+
+    let file_descriptors = protox::compile(&PROTO_FILES, &PROTO_INCLUDES)?;
+
+    let file_descriptor_path =
+        PathBuf::from(env::var_os("OUT_DIR").unwrap()).join("file_descriptor_set.bin");
+    fs::write(&file_descriptor_path, file_descriptors.encode_to_vec())?;
 
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
         .file_descriptor_set_path(&file_descriptor_path)
         .skip_protoc_run()
-        .compile_protos(&["occurrence/v1/occurrences_service.proto"], &["./protos"])
-        .unwrap();
+        .compile_protos(&PROTO_FILES, &PROTO_INCLUDES)?;
+
+    Ok(())
 }
